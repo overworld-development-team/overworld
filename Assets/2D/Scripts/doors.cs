8bbec950df60177ff855cfde6d877850f4fc6e71
yfkuang <contact@yfkuang.com>
2019-03-06 00:53:39 -0500

    Alpha prototype

--- Assets/2D/Scripts/doors.cs
+++ Assets/2D/Scripts/doors.cs
@@ -6,20 +6,43 @@ using UnityEngine.UI;
 
 public class doors : MonoBehaviour {
 
-	void Start () {
-		
+    public GameObject rotator1;
+    public GameObject rotator2;
+    public GameObject rotator3;
+    public GameObject rotator4;
+    public GameObject character;
+    public RawImage bg;
+    public Texture doorclosed;
+    public Texture dooropen;
+    public Texture overworld;
+
+    void Start () {
+        bg.texture = doorclosed;
 	}
 
 	void Update () {
-		
-	}
+        if (rotator1.transform.eulerAngles.x > -15 && rotator1.transform.eulerAngles.x < 15)
+        {
+            Debug.Log("Rotator 1 true");
+            if (rotator2.transform.eulerAngles.x > -15 && rotator2.transform.eulerAngles.x < 15)
+            {
+                Debug.Log("Rotator 2 true");
+                if (rotator3.transform.eulerAngles.x > -15 && rotator3.transform.eulerAngles.x < 15)
+                {
+                    Debug.Log("Rotator 3 true");
+                    if (rotator4.transform.eulerAngles.x > -15 && rotator4.transform.eulerAngles.x < 15)
+                    {
+                        Debug.Log("Rotator 4 true");
+                        bg.texture = dooropen;
+                    } else { Debug.Log("Rotator 4 false"); }
+                } else { Debug.Log("Rotator 3 false"); }
+            } else { Debug.Log("Rotator 2 false"); }
+        } else { Debug.Log("Rotator 1 false"); }
 
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        Debug.Log("COLLIDE");
-        if (Input.GetKey(KeyCode.Return) && col.tag == "bedroomDoor") {
-            CanvasGroup room1 = GetComponent<CanvasGroup>();
-            room1.alpha = 0f;
+        if (OVRInput.Get(OVRInput.Button.Two) && character.transform.position.x > 700)
+        {
+            bg.texture = overworld;
+            // THIS IS WHERE IT CHANGES THE ROOM TO OVERWORLD TITLE SCREEN
         }
-    }
+	}
 }
---
+++ Assets/3D/Scripts/TriggerFingers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFingers : MonoBehaviour {
+    public Collider leftHand;
+    public Collider rightHand;
+
+    void Start () { }
+
+	void Update () {
+        if (OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger, OVRInput.Controller.LTouch)) {
+            Debug.Log("(LEFT) Not pointing");
+            leftHand.isTrigger = true;
+        } else {
+            Debug.Log("(LEFT) Pointing");
+            leftHand.isTrigger = false;
+        }
+
+        if (OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger, OVRInput.Controller.RTouch)) {
+            Debug.Log("(RIGHT) Not pointing");
+            rightHand.isTrigger = true;
+        } else {
+            Debug.Log("(RIGHT) Pointing");
+            rightHand.isTrigger = false;
+        }
+    }
+}
